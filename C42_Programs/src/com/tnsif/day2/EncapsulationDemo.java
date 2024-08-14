848fb33823802db6ee48235f1dc2e035e3aa8538
babukalai <babukalai768@gmail.com>
2024-08-14 11:07:58 +0530

    commit 2

---
+++ C42_Programs/src/com/tnsif/day2/EncapsulationDemo.java
@@ -0,0 +1,38 @@
+package com.tnsif.day2;
+
+public class EncapsulationDemo {
+	//Team RCB
+	
+	String playerName ;
+	int orderNo ;
+	int jersyNo ;
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public int getOrderNo() {
+		return orderNo;
+	}
+
+	public void setOrderNo(int orderNo) {
+		this.orderNo = orderNo;
+	}
+
+	public int getJersyNo() {
+		return jersyNo;
+	}
+
+	public void setJersyNo(int jersyNo) {
+		this.jersyNo = jersyNo;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
